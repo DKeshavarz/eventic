acb70f97c9d5835f758cd3aa1cbc02e8f631028e
DKeshavarz <dankeshavarz1075@gmail.com>
2025-11-22 23:41:15 +0330

    chore: add webserver basics

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	corsConfig := cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+	}
+
+	engine.Use(cors.New(corsConfig))
+	
+	engine.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status" : "ok",
+			"time" : time.Now(),
+			"service" : "eventic v0.0.0",
+		})
+	})
+	log.Println(engine.Run(":8080"))
+}
