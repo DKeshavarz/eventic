1af9c9f56dc1f22f07c0f2bf089bcd7d98bb517e
DKeshavarz <dankeshavarz1075@gmail.com>
2025-11-26 00:56:03 +0330

    chore: add config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,24 @@
 package config
 
+import (
+	"os"
+
+	"github.com/DKeshavarz/eventic/internal/delivery/telegram"
+)
+
 type Config struct {
+	Telegram *telegram.Config
 }
 
-
-func New() (*Config, error) {
-	config := Config{
-		
+func New() *Config {
+	config := &Config{
+		Telegram: telegram.DefaultConfig(),
 	}
 
-	return &config, nil
+	loadTelegram(config.Telegram)
+	return config
+}
+
+func loadTelegram(cfg *telegram.Config) {
+	cfg.APIKey = os.Getenv("TELEGRAM_API_KEY")
 }
