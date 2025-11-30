a434c27f12f66ef2f5e416caacd1c1d447e11b33
DKeshavarz <dankeshavarz1075@gmail.com>
2025-12-01 01:15:51 +0330

    chore: add validate to Organization

--- internal/entity/organization.go
+++ internal/entity/organization.go
@@ -1,5 +1,11 @@
 package entity
 
+import (
+	"errors"
+
+	"github.com/DKeshavarz/eventic/internal/entity/validation"
+)
+
 type Organization struct {
 	ID          int     `json:"organizer_id"`
 	OwnerID     int     `json:"id"`
@@ -8,4 +14,31 @@ type Organization struct {
 	LogoPic     *string `json:"logo_pic"`
 	Email       *string `json:"email"`
 	Phone       *string `json:"phone"`
+}
+
+var (
+	ErrInvalidDescription = errors.New("invalid description")
+	ErrInvalidName        = errors.New("invalid name")
+	ErrInvalidEmail       = errors.New("invalid email")
+	ErrInvalidPhone       = errors.New("invalid phone")
+)
+
+func (org *Organization) Validate() error {
+	if len(org.Description) == 0 {
+		return ErrInvalidDescription
+	}
+
+	if len(org.Name) == 0 {
+		return ErrInvalidName
+	}
+
+	if org.Email != nil && validation.Email(*org.Email) != nil {
+		return ErrInvalidEmail
+	}
+
+	if org.Phone != nil && validation.Phone(*org.Phone) != nil {
+		return ErrInvalidPhone
+	}
+
+	return nil
 }
