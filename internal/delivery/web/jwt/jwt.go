5775ba5256f7d6325d2840a72db53efb8a0a8002
DKeshavarz <dankeshavarz1075@gmail.com>
2025-12-01 22:22:32 +0330

    chore: add jwt auth

---
+++ internal/delivery/web/jwt/jwt.go
@@ -0,0 +1,49 @@
+package jwt
+
+import (
+	"errors"
+	"time"
+
+	"github.com/DKeshavarz/eventic/internal/entity"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("I just want you to know that i love you")
+
+type Claims struct {
+	UserID int `json:"userid"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(user *entity.User, duration time.Duration) (string, error) {
+	claims := &Claims{
+		UserID: user.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(duration)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return jwtToken.SignedString(jwtSecret)
+}
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
---
+++ internal/delivery/web/jwt/jwt_test.go
@@ -0,0 +1,54 @@
+package jwt
+
+import (
+    "testing"
+    "time"
+
+    "github.com/DKeshavarz/eventic/internal/entity"
+    "github.com/stretchr/testify/assert"
+)
+
+func TestGenerateJWT(t *testing.T) {
+    user := &entity.User{ID: 123}
+    duration := time.Hour
+
+    token, err := GenerateJWT(user, duration)
+    assert.NoError(t, err)
+    assert.NotEmpty(t, token)
+}
+
+func TestValidateJWT(t *testing.T) {
+    user := &entity.User{ID: 123}
+    duration := time.Hour
+
+    // Generate a token
+    token, err := GenerateJWT(user, duration)
+    assert.NoError(t, err)
+
+    // Validate the token
+    claims, err := ValidateJWT(token)
+    assert.NoError(t, err)
+    assert.Equal(t, user.ID, claims.UserID)
+}
+
+func TestValidateJWT_InvalidToken(t *testing.T) {
+    // Test with an invalid token
+    invalidToken := "invalid.token.string"
+    claims, err := ValidateJWT(invalidToken)
+    assert.Error(t, err)
+    assert.Nil(t, claims)
+}
+
+func TestValidateJWT_ExpiredToken(t *testing.T) {
+    user := &entity.User{ID: 123}
+    duration := -time.Hour // Token already expired
+
+    // Generate an expired token
+    token, err := GenerateJWT(user, duration)
+    assert.NoError(t, err)
+
+    // Validate the expired token
+    claims, err := ValidateJWT(token)
+    assert.Error(t, err)
+    assert.Nil(t, claims)
+}
