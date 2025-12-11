1e393ef1c1fb683b1cc47cf56678ca7d999beb43
DKeshavarz <dankeshavarz1075@gmail.com>
2025-12-11 17:27:01 +0330

    Refactor: seprate dto frome codes

--- internal/delivery/web/auth/handlers.go
+++ internal/delivery/web/auth/handlers.go
@@ -6,29 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type LoginWithPhoneRequest struct {
-	Phone    string `json:"phone"`
-	Password string `json:"password"`
-}
-
-type RefreshTokenRequest struct {
-	RefreshToken string `json:"refresh_token"`
-}
-
-type LoginWithEmailRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type LoginResponse struct {
-	Token        string `json:"token"`
-	RefreshToken string `json:"refresh_token"`
-}
-type ErrorResponse struct {
-	Error string `json:"error"`
-	Meta  string `json:"meta,omitempty"`
-}
-
 // Login        godoc
 // @Summary     login a user with Eamil
 // @Description Login a user with Eamil to app and generate a hwt token and a jwt refresh token
