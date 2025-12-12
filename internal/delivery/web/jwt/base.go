444775d101ba058b8e29055fd4bb330019f3e7ee
DKeshavarz <dankeshavarz1075@gmail.com>
2025-12-13 01:20:12 +0330

    chore: add jwt base

---
+++ internal/delivery/web/jwt/base.go
@@ -0,0 +1,43 @@
+package jwt
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type TokenSigner interface {
+	Sign(claims jwt.Claims) (string, error)
+	Parse(tokenString string, claims jwt.Claims) error
+}
+
+const (
+	TokenTypeAccess       = "access"
+	TokenTypeConfirmation = "confirmation"
+)
+
+// ------------------------- imp ------------------------
+
+func NewHMACSigner(secret []byte) TokenSigner {
+	return &hmacSigner{secret: secret}
+}
+
+type hmacSigner struct {
+	secret []byte
+}
+
+func (s *hmacSigner) Sign(claims jwt.Claims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(s.secret)
+}
+
+func (s *hmacSigner) Parse(tokenString string, claims jwt.Claims) error {
+	_, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return s.secret, nil
+	})
+	return err
+}
