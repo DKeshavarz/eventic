3362c78c95422034057b5c16d6fee3de7d6dc3f6
DKeshavarz <dankeshavarz1075@gmail.com>
2025-12-14 11:38:47 +0330

    Chore: wiring stuff

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"github.com/DKeshavarz/eventic/internal/config"
 	"github.com/DKeshavarz/eventic/internal/delivery"
+	"github.com/DKeshavarz/eventic/internal/getways/mail"
+	"github.com/DKeshavarz/eventic/internal/repositories/cache"
 	"github.com/DKeshavarz/eventic/internal/repositories/inmemory"
+	"github.com/DKeshavarz/eventic/internal/usecase/auth"
 	"github.com/DKeshavarz/eventic/internal/usecase/user"
 )
 
@@ -11,13 +14,15 @@ func main() {
 	cfg := config.New()
 
 	db := inmemory.DefaultDB()
-
+	cache := cache.New()
+	sender := mail.New(cfg.Mail)
 	userStorage := inmemory.NewUserStorage(db)
+
 	// orgStorage := inmemory.NewOrgStorage(db)
 	// eventStorage := inmemory.NewEventStorage(db)
 	// joinEventStorage := inmemory.NewJoinEventStorage(db)
 
 	userSevice := user.NewSevice(userStorage)
-
-	delivery.Start(cfg.Delivery, userSevice)
+	authService := auth.New(cache, sender)
+	delivery.Start(cfg.Delivery, userSevice, authService)
 }
