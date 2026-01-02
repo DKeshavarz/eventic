64e61abbab5a77a6233dfc675b57d74f491d0960
DKeshavarz <dankeshavarz1075@gmail.com>
2026-01-02 23:46:50 +0330

    chore: add file founder

---
+++ pkg/utile/utile_test.go
@@ -0,0 +1,30 @@
+package utile
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStrPtr(t *testing.T) {
+	input := "test"
+	result := StrPtr(input)
+
+	assert.NotNil(t, result)
+	assert.Equal(t, input, *result)
+}
+
+func TestFileExists_Basic(t *testing.T) {
+	nonExistent := "/tmp/this-file-should-not-exist-12345"
+	res := FileExists(nonExistent)
+	assert.False(t, res)
+
+	tmpfile, err := os.CreateTemp("", "testfile")
+	assert.Nil(t, err)
+	defer os.Remove(tmpfile.Name())
+	defer tmpfile.Close()
+
+	res = FileExists(tmpfile.Name())
+	assert.True(t, res)
+}
